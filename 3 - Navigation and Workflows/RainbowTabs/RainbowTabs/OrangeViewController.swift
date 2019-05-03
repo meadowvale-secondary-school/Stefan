9bd1a5875bc2756c71a90bfe45fcae9e04b9fba5
Stefan Georgiev <594688@pdsb.net>
2019-05-03 15:36:01 -0400

    Committing lesson and lab 3.7 Tab Bar Controllers

---
+++ 3 - Navigation and Workflows/RainbowTabs/RainbowTabs/OrangeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OrangeViewController.swift
+//  RainbowTabs
+//
+//  Created by Student2562 on 2019-05-03.
+//  Copyright © 2019 Student2562. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 3 - Navigation and Workflows/RainbowTabs/RainbowTabs/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Student2562 on 2019-05-03.
+//  Copyright © 2019 Student2562. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tabBarItem.badgeValue = "!"
+        tabBarItem.badgeValue = nil
+    }
+
+
+}
+
