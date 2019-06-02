58a9190bb0d95195aaf69d32f15bbc1d52480400
Stefan Georgiev <594688@pdsb.net>
2019-06-02 11:21:53 -0400

    4.5 EmojiDictionary Lesson - Fixed useless delete UI and tab Bar Button changes from edit to done

--- 4 - Tables and Persistence/EmojiDictionary/EmojiDictionary/EmojiTableViewControllerV2.swift
+++ 4 - Tables and Persistence/EmojiDictionary/EmojiDictionary/EmojiTableViewControllerV2.swift
@@ -58,6 +58,7 @@ class EmojiTableViewControllerV2: UITableViewController {
     @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
         let tableViewEdittingMode = tableView.isEditing
         tableView.setEditing(!tableViewEdittingMode, animated: true)
+        sender.title = !tableViewEdittingMode ? "Done" : "Edit"
     }
     
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath){
@@ -65,49 +66,17 @@ class EmojiTableViewControllerV2: UITableViewController {
         emojis.insert(movedEmoji, at: to.row)
         tableView.reloadData()
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle{
+        return UITableViewCell.EditingStyle.none
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath : IndexPath) -> Bool{
+        return false
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
